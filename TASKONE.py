431e8a1f5a16151781d6179aead39f41dfd3f6af
daxilbhuva <shaktijay40@gmail.com>
2026-01-21 08:39:31 +0530

    Add files via upload

---
+++ TASKONE.py
@@ -0,0 +1,62 @@
+#que-1
+
+p=float(input("enter principal : "))
+r=float(input("enter rate : "))
+t=float(input("enter time : "))
+         
+si=(p*r*t)/100
+print(si)
+
+#que-2
+
+for i in renge(1,6);
+print(i)
+
+#que-3
+
+a=int(input("enter a :"))
+b=int (input("enter b :"))
+
+print(max(a,b))
+
+#que-4
+
+s=input("enter a strong :")
+print("length",len(s))
+
+#que-5
+
+print("welcome to python programming")
+
+#que-6
+
+s=input("enter a string :")
+print("first character=",s[0])
+
+#que-7
+
+n=int(input("enter a number:"))
+if n>0:
+    print("positive number")
+elif n<0:
+    print("negative number")
+else:
+    print("zero")
+    
+#que-8
+
+s=input("enter a string :")
+print("last character=",s[-1])
+
+#que-9
+
+a=int(input("enter a :"))
+b=int(input("enter b:"))
+c=int(input("enter c :"))
+
+print("sum=",a+b+c)
+
+#que-10
+
+n=int(input("enter a number :"))
+print("square=",n*n)
