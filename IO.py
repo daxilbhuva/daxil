7ed7642001c23ddb9695333e8e7e1fe3715f110c
daxilbhuva <shaktijay40@gmail.com>
2026-02-02 10:40:36 +0530

    Add files via upload

---
+++ IO.py
@@ -0,0 +1,56 @@
+#output statment:
+
+#int
+print (500000)  #1
+
+print (489654)  #2
+
+#float
+print (143.2)   #1
+
+print (156.36)  #2
+
+#bool
+is_pass=True
+print (is_pass) #1
+
+is_fail=False
+print (is_fail) #2
+
+#str
+a=("daxuu")
+print (a)   #1
+
+b=("bhuva")
+print (b)   #2
+
+#input stetment:
+#int
+number=int(input("enter number:"))  #1
+print ("Number:",number)
+
+number=int(input("enter number:"))  #2
+print ("Number:",number)
+
+#float
+num=float(input("enter number:"))   #1
+print ("float number:",num)
+
+num=float(input("enter number:"))   #2
+print ("float number:",num)
+
+#bool
+
+d = input("You are student")    #1
+print(d)
+
+a = input("You are student")    #2
+print(a)
+
+#str
+
+name=str(input("enter name :")) #1
+print ("hello",name)
+
+city=str(input("enter city :")) #2
+print ("lieve in",city)
