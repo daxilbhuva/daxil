371f2c216df5f77fab3a52f265a897bca009d3c7
daxilbhuva <shaktijay40@gmail.com>
2026-02-18 08:11:50 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,46 @@
+
+    
+    # 1. Print numbers from 1 to 10
+for i in range(1, 11):
+    print(i)
+
+# 2. Print even numbers from 1 to 20
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)
+
+# 3. Print odd numbers from 1 to 15
+for i in range(1, 16):
+    if i % 2 != 0:
+        print(i)
+
+# 4. Print characters of a string
+name = "atmiya"
+for letter in name:
+    print(letter)
+
+#5 Print table of 5
+for i in range(1, 11): 
+    print(f"5 x {i} = {5 * i}")  
+    
+#6 Sum of numbers from 1 to 5
+total = 0
+for i in range(1, 6):  
+    total += i  
+    
+#7 Example list
+my_list = [10, 20, 30, 40, 50]
+
+for element in my_list:
+    print(element)
+
+#8 print numbers from 1 to 5
+
+for i in range (1,6):
+    print(i)
+
+#9 print a massage 3 time
+
+for i in range (3):
+    print(hello)
+    
